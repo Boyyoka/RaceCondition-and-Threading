d2d103dabf752bad1270f19a7adf5a67fb966582
Paaldanzen Danzma <boykehooplot@hotmail.com>
2016-12-02 01:19:00 +0900

    initial commit

---
+++ src/BankAccount.java
@@ -0,0 +1,56 @@
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+   A bank account has a balance that can be changed by 
+   deposits and withdrawals.
+*/
+public class BankAccount
+{
+   //private Lock balanceChangeLock;
+   private double balance;
+
+   /**
+      Constructs a bank account with a zero balance.
+   */
+   public BankAccount()
+   {
+      balance = 0;
+      //balanceChangeLock = new ReentrantLock();
+   }
+
+   /**
+      Deposits money into the bank account.
+      @param amount the amount to deposit
+   */
+   public void deposit(double amount)
+   {
+      System.out.print("Depositing " + amount);
+      double newBalance = balance + amount;
+      System.out.println(", new balance is " + newBalance);
+      balance = newBalance;
+   }
+   
+   /**
+      Withdraws money from the bank account.
+      @param amount the amount to withdraw
+   */
+   public void withdraw(double amount)
+   {     
+      System.out.print("Withdrawing " + amount);
+      double newBalance = balance - amount;
+      System.out.println(", new balance is " + newBalance);
+      balance = newBalance;
+   }
+   
+   /**
+      Gets the current balance of the bank account.
+      @return the current balance
+   */
+   public double getBalance()
+   {
+      return balance;
+   }
+
+}
